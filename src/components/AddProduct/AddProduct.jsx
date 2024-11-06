def29d727ccbda59599e28baed5b119135952b5e
Faiz Mohd <42231693+ifaizmohd@users.noreply.github.com>
2024-11-07 01:43:00 +0530

    created add product page

---
+++ src/components/AddProduct/AddProduct.jsx
@@ -0,0 +1,95 @@
+import { Typography } from "@mui/material";
+import PageLayout from "../../common/components/PageLayout/PageLayout";
+import Form from "../../common/components/Form/Form";
+import { useContext, useState } from "react";
+import { ProductsApi } from "../../common/api";
+import { useNotification } from "../../common/Provider/Notification.provider";
+import { checkForSessionErrors } from "../../common/lib/utils";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../common/Provider/Auth.context";
+
+const AddProduct = () => {
+  const fields = [
+    {
+      type: "text",
+      label: "Name",
+      name: "name",
+    },
+    {
+      type: "text",
+      label: "Category",
+      name: "category",
+    },
+    {
+      type: "number",
+      label: "Price",
+      name: "price",
+    },
+    {
+      type: "text",
+      label: "Description",
+      name: "description",
+    },
+    {
+      type: "text",
+      label: "Manufacturer",
+      name: "manufacturer",
+    },
+    {
+      type: "text",
+      label: "Image URL",
+      name: "imageUrl",
+    },
+    {
+      type: "number",
+      label: "Available Items",
+      name: "availableItems",
+    },
+  ];
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "",
+    price: 0,
+    description: "",
+    manufacturer: "",
+    imageUrl: "",
+    availableItems: 0,
+  });
+
+  const { showNotification } = useNotification();
+  const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
+
+  const handleFormChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await ProductsApi.createProduct(formData);
+      checkForSessionErrors(error, showNotification, navigate, logout);
+      if (data === 200) {
+        showNotification("Product added successfully", "success");
+        navigate("/products"); // if product is created successfully then navigating the user to products page
+      }
+    } catch (error) {
+      showNotification("Please provide all valid details of product", "error");
+    }
+  };
+
+  return (
+    <PageLayout containerSize="sm" topMargin="4em">
+      <Typography>Add Product</Typography>
+      <Form
+        fields={fields}
+        buttonCta="SAVE PRODUCT"
+        formData={formData}
+        formHandler={handleFormChange}
+        handleSubmit={handleFormSubmit}
+      />
+    </PageLayout>
+  );
+};
+
+export default AddProduct;
